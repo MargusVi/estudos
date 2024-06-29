a0f3e5c3a1e41b0504ac3ea495629369cb30b2ba
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2024-06-29 10:48:30 -0300

    -Rust- Hello World!

---
+++ Rust/helloWorld/target/debug/deps/helloWorld.d
@@ -0,0 +1,5 @@
+C:\Users\marco\Documents\Desenvolvimento\Estudos\Rust\helloWorld\target\debug\deps\helloWorld.exe: src\main.rs
+
+C:\Users\marco\Documents\Desenvolvimento\Estudos\Rust\helloWorld\target\debug\deps\helloWorld.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/helloWorld/target/debug/helloWorld.d
@@ -0,0 +1,1 @@
+C:\Users\marco\Documents\Desenvolvimento\Estudos\Rust\helloWorld\target\debug\helloWorld.exe: C:\Users\marco\Documents\Desenvolvimento\Estudos\Rust\helloWorld\src\main.rs
